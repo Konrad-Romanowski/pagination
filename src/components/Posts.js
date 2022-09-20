e671848b230aa368d5513950f16011aa39203a32
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2022-09-20 20:11:54 +0200

    Added option to change number of posts per page, merged states into one

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,13 +2,35 @@ import React from 'react';
 import '../App.css';
 
 export default function Posts({posts}) {
-    const [postsPerPage, setPostsPerPage] = React.useState(10);
-    const [currentPage, setCurrentPage] = React.useState(1);
 
-    const firstPostIndex = (currentPage - 1) * postsPerPage;
-    const lastPostIndex = firstPostIndex + postsPerPage;
+    const [pagination, setPagination] = React.useState({
+        postsPerPage: 10,
+        currentPage: 1,
+        firstPostIndex: 0,
+        lastPostIndex: 10
+    });
 
-    const currentPosts = posts.slice(firstPostIndex,lastPostIndex);
+    function changeCurrentPage(number) {
+        setPagination(prevPagination => {
+            return {
+                ...prevPagination,
+                currentPage: number,
+                firstPostIndex: (number - 1) * prevPagination.postsPerPage,
+                lastPostIndex: (number - 1) * prevPagination.postsPerPage + prevPagination.postsPerPage
+            }
+        })
+    }
+    
+    function changePostsPerPage(number) {
+        setPagination({
+            currentPage: 1,
+            postsPerPage: number,
+            firstPostIndex: 0,
+            lastPostIndex: number
+        })
+    }
+    
+    const currentPosts = posts.slice(pagination.firstPostIndex,pagination.lastPostIndex);
 
     const postsElements = currentPosts.map(post=>
         <div className='post' key={post.id}>
@@ -17,15 +39,16 @@ export default function Posts({posts}) {
         </div>
     )
 
-    function changeCurrentPage(number) {
-        setCurrentPage(number);
-    }
+    const numberOfPages = Math.ceil(posts.length / pagination.postsPerPage);
 
-    const numberOfPages = Math.ceil(posts.length / postsPerPage);
     const paginationElements = [];
     for(let i = 1; i <= numberOfPages; i++) {
         paginationElements.push(
-            <button className={i === currentPage ? 'page-button active-page' : 'page-button'} key={i} onClick={() => changeCurrentPage(i)}>
+            <button 
+                className={i === pagination.currentPage ? 'page-button active-page' : 'page-button'}
+                key={i}
+                onClick={() => changeCurrentPage(i)}
+            >
                 {i}
             </button>
         )
@@ -39,6 +62,15 @@ export default function Posts({posts}) {
 
             <section id="posts-pagination" className='posts-pagianition'>
                 {paginationElements}
+                <select
+                    name="postsPerPage"
+                    value={pagination.postsPerPage}
+                    onChange={(e)=>changePostsPerPage(parseInt(e.target.value))}
+                >
+                    <option value={5}>5</option>
+                    <option value={10}>10</option>
+                    <option value={25}>25</option>
+                </select>
             </section>
         </>
     )
